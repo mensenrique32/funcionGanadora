e2f08ede71b1b5385cb7233661388e6e2483e2c9
Iván <110126473+mensenrique32@users.noreply.github.com>
2022-07-27 16:11:17 -0300

    Add files via upload

---
+++ la función ganadora.py
@@ -0,0 +1,11 @@
+def funcionGanadora():
+    print("¡Ganaste!")
+    
+print("Ingrese y para ganar:")
+respuesta = str(input())
+
+if (respuesta == "y"):
+    funcionGanadora()
+else:
+    print("pelotudo")
+
